135a7c030c7d055eef6310e2fd795f33d1b74b9e
dhruvamungamuri <dhruvamungamuri@gmail.com>
2023-10-13 12:37:21 +0530

    Changed code for manual approval message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Dhruva Mungamuri!'));
+app.get('/', (req, res) => res.send('Hello Dhruva - Please approve!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
